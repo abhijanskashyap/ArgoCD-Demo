469963b0b5360f235da85e9284c3dcae283c81c4
Abhijan <abhijanskashyap@gmail.com>
2026-06-15 16:45:12 +0530

    Changes v2

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello from ArgoCD GitOps Demo!"
+    return "Hello from ArgoCD GitOps Demo!----version-2"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
