0983824d0246ca65860b01e12c0099a9927527d3
Abhijan <abhijanskashyap@gmail.com>
2026-06-15 17:34:50 +0530

    Release v3 dashboard

--- app.py
+++ app.py
@@ -1,10 +1,18 @@
 from flask import Flask, render_template
+import os
+import socket
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template(
+        "index.html",
+        hostname=socket.gethostname(),
+        version="v3.0",
+        environment="Development",
+        app_name="Flask GitOps Demo"
+    )
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
